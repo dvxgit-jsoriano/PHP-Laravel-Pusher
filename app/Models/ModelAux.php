74b642a865101e87e16f9fc91b1cd8283b5b89cf
dvxgit-jsoriano <jsoriano@diavox.net>
2022-09-12 11:35:32 +0800

    ModelAux

---
+++ app/Models/ModelAux.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelAux extends Model
+{
+    public $table = "auxes";
+
+    use HasFactory;
+}
